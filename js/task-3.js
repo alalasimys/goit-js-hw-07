d045b54fe201343e1a6de5b863b3fe95647a45a4
Alla Karhopolova <alalasimys@gmail.com>
2021-02-25 21:41:44 +0200

    add completed task 3

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const galleryRef = document.querySelector("#gallery");
+
+const createImgEl = ({ url, alt }) =>
+  `<li class="gallery-item"><img src="${url}" alt="${alt}"/></li>`;
+
+const createLayout = (options) => {
+  return options.map(createImgEl).join("");
+};
+
+galleryRef.insertAdjacentHTML("afterbegin", createLayout(images));
